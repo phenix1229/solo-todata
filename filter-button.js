a2d1a0f8b9091d05bfc8ea8291d17ac7e5e9e15d
phenix1229 <paul.garay@codeimmersives.com>
2019-12-05 21:02:35 -0500

    changed multiple functions

--- filter-button.js
+++ filter-button.js
@@ -1,15 +1,15 @@
 const filterOption = document.querySelector('.filter');
 
 document.querySelector('.filter-todo').addEventListener('click', () => {
+    clearCurrentTodos();
     if (filterOption.value === 1){
         printTodos(todos);
-    } else if (filterOption.value === '2'){
-        clearCurrentTodos();
-        // todos.forEach(todo => {
-        //     if (todo.complete === false){
-        //         currentTodos.push(todo);
-        //     }
-        // })
+    } else if (filterOption.value === 2){
+        todos.forEach(todo => {
+            if (todo[complete] === false){
+                currentTodos.push(todo);
+            }
+        })
         printTodos(currentTodos)
     }
 }
